b56598a14dc3e8251a76c00e03880c447a5cce88
TheArabicGodfather <aaiden798@gmail.com>
2023-08-09 16:37:40 -0400

    further fixation nation make hero very adaptation

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -5,17 +5,17 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <div className="hero h-screen text-accent-content">
-      <div className="min-w-fit w-3/4 md:flex md:items-center md:justify-center card -translate-y-10 z-10">
-        <div className="grid gap-4 text-center md:text-left ">
-          <div className="text-4xl md:text-7xl font-extrabold text-neutral stroke">
+      <div className="min-w-fit w-3/4 flex items-center justify-center card -translate-y-10 z-10">
+        <div className="grid gap-4 text-center">
+          <div className="text-3xl md:text-5xl lg:text-7xl font-extrabold text-neutral stroke">
             Peaceful Escapes
           </div>
-          <div className="text-4xl md:text-7xl font-extrabold text-neutral stroke">
+          <div className="text-3xl md:text-5xl lg:text-7xl font-extrabold text-neutral stroke">
             Accommodation
           </div>
           <br />
           <Link href="/about">
-            <button className="btn btn-primary btn-wide text-primary-content">
+            <button className="btn btn-primary btn-wide text-primary-content text-sm md:text-base lg:text-lg">
               Learn More
             </button>
           </Link>
